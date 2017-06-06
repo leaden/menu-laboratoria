b2025458ec6a9b5026a241affc2a4e7e82ddb2a7
maribel <maribel.diaz.j@gmail.com>
2017-06-06 13:49:53 -0400

    menu ham

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*var lista = document.getElementByClassName("lista-desple");
+var icono = document.getElementById("icono");
+
+icono.addEventListener("click", desplegable);
+	
+
+function desplegable(){
+		alert("hola");
+	}
+
+	*/
